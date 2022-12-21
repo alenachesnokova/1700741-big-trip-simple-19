fbf45c3b5922c7ffae4c22b19b7a42c6041bb9b4
alenachesnokova <chesnokovaeg@mail.ru>
2022-12-21 17:13:43 +0500

    недоделанная модель

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import FiltersView from './views/filters-view';
 import { render } from './render';
 import ListPresenter from './presenters/list-presenter';
+import PointsModel from './models/point-model';
+
 
 const siteMainElement = document.querySelector('.page-main');
 const siteFiltersElement = document.querySelector('.trip-controls');
 const siteContentElement = siteMainElement.querySelector('.trip-events');
-const listPresenter = new ListPresenter({listContainer: siteContentElement});
+
+const pointsModel = new PointsModel();
+const listPresenter = new ListPresenter({
+  listContainer: siteContentElement,
+  pointsModel,
+});
 
 render(new FiltersView(), siteFiltersElement);
 
 listPresenter.init();
+console.log(listPresenter.init());
--- src/presenters/list-presenter.js
+++ src/presenters/list-presenter.js
@@ -7,17 +7,21 @@ import { render } from '../render';
 export default class ListPresenter {
   listComponent = new ListView();
 
-  constructor({listContainer}) {
+  constructor({listContainer, pointsModel}) {
     this.listContainer = listContainer;
+    this.pointsModel = pointsModel;
+    console.log(this.pointsModel);
   }
 
   init() {
+    this.listPoints = [...this.pointsModel.getPoints()];
+
     render(this.listComponent, this.listContainer);
     render(new SortingView, this.listComponent.getElement());
     render(new NewPointEditorView, this.listComponent.getElement());
 
     for (let i = 0; i < 3; i++) {
-      render(new PointView, this.listComponent.getElement());
+      render(new PointView({point: this.listPoints[i]}), this.listComponent.getElement());
     }
   }
 }
